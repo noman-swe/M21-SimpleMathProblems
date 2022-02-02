8cedbcbf699cdbf6b144775cc56070be425fb008
noman-swe <noman.swe130@gmail.com>
2022-02-03 00:57:03 +0600

    21-2:Swapping value of var

---
+++ 2-swap.js
@@ -0,0 +1,7 @@
+var first = 5;
+var second = 7;
+console.log(first, second); //output: 5 7
+var temp = first;///first er value temp e rakhlam
+first = second; //second er value first e rakhlam
+second = temp; //temp e thaka first er value second e nilam
+console.log(first, second); // output: 7 5
