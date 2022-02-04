ea95d4db1155051863e4085dd754d712d38da1d3
noman-swe <noman.swe130@gmail.com>
2022-02-04 14:54:22 +0600

    fibonacchi-something went worng

---
+++ Fibonacci.js
@@ -0,0 +1,12 @@
+//0, 1, 1, 2, 3, 4, 5, 8, 13, 21, 34, 55;
+/* 
+nth = (n-1)th + (n-2)th
+*/
+
+const fibo = [0, 1];
+for(let i = 2; i <= 10; i++){
+
+fibo[i] = fibo[i - 1] + fibo[i -2];
+
+}
+console.log(fibo); 
