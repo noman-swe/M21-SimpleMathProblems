fc5d8fbce42619618f4d55dd51926f864157f146
noman-swe <noman.swe130@gmail.com>
2022-02-04 15:04:42 +0600

    fibo function

--- Fibonacci.js
+++ Fibonacci.js
@@ -3,10 +3,24 @@
 nth = (n-1)th + (n-2)th
 */
 
+/*
 const fibo = [0, 1];
 for(let i = 2; i <= 10; i++){
 
-fibo[i] = fibo[i - 1] + fibo[i -2];
+fibo[i] = fibo[i - 1] + fibo[i - 2];
 
 }
 console.log(fibo); 
+*/
+/*
+function fibonacchiSeries(num){
+    const fibo = [0, 1];
+    for(let i = 2; i <= 10; i++){
+        fibo[i] = fibo[i - 1] + fibo[i - 2];    
+    }
+    return fibo;
+}
+
+const fibonacchi = fibonacchiSeries(10);
+console.log(fibonacchi);
+*/
